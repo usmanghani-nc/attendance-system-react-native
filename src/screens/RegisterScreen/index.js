86282eeb35e8a878b2b0726daed667c86aafb1f9
usmanGhani16 <afro123737@gmail.com>
2021-02-05 22:34:45 +0500

    testing

--- src/screens/RegisterScreen/index.js
+++ src/screens/RegisterScreen/index.js
@@ -3,7 +3,7 @@ import { View, Text } from 'react-native';
 
 export default function RegisterScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{ background: 'coral', flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Register</Text>
       <Text onPress={() => navigation.navigate('Login')}>Login</Text>
     </View>
