6e8743b7f25cd6da96575bbbc60206f6ddb7eee6
usmanGhani16 <afro123737@gmail.com>
2021-06-22 01:21:21 +0500

    changes

--- attendance-systen-backend/test.js
+++
@@ -1,9 +0,0 @@
-const list = new Array(60000).join('1.1').split('.');
-
-function removeItemsFromList() {
-  var item = list.pop();
-
-  console.log(list.length, 'ITEM');
-}
-
-removeItemsFromList();
